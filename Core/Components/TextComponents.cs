596651058eda13a807a8b16adac88482355a9b4c
Porrith Suong <porrith.suong@gmail.com>
2020-01-07 11:15:26 -0500

    Begins adding the TextConversionSystem.

--- Core/Components/TextComponents.cs
+++ Core/Components/TextComponents.cs
@@ -1,25 +1,20 @@
-using Unity.Collections;
 using Unity.Entities;
+using UnityEngine.TextCore;
 
 namespace UGUIDots {
 
-    public struct Text32 : IComponentData {
-        public NativeString32 Value;
+    // TODO: Represent chars as ushort instead?
+    public struct TextElement : IBufferElementData {
+        public char Value;
+
+        public static implicit operator TextElement(char value) => new TextElement { Value = value };
+        public static implicit operator char(TextElement value) => value.Value;
     }
 
-    public struct Text64 : IComponentData {
-        public NativeString64 Value;
-    }
-
-    public struct Text128 : IComponentData {
-        public NativeString128 Value;
-    }
-
-    public struct Text512 : IComponentData {
-        public NativeString512 Value;
-    }
-
-    public struct Text4096 : IComponentData {
-        public NativeString512 Value;
+    public struct GlyphElement : IBufferElementData {
+        public ushort Char;
+        public float Scale;
+        public GlyphRect GlyphRect;
+        public GlyphMetrics Metrics;
     }
 }
--- Core/Systems/Conversions/TextConversionSystem.cs
+++ Core/Systems/Conversions/TextConversionSystem.cs
@@ -1,16 +1,50 @@
-﻿using TMPro;
+﻿using System.Collections.Generic;
+using TMPro;
 using UGUIDots.Render;
 using Unity.Entities;
-
+using UnityEngine.TextCore;
 namespace UGUIDots.Conversions.Systems {
 
-    public class TextConversionSystem : GameObjectConversionSystem {
+    [UpdateInGroup(typeof(GameObjectDeclareReferencedObjectsGroup))]
+    public class TMPFontAssetDeclareSystem : GameObjectConversionSystem {
+        protected override void OnUpdate() {
+            Entities.ForEach((TextMeshProUGUI c0) => {
+                DeclareReferencedAsset(c0.font);
+            });
+        }
+    }
+
+    [UpdateInGroup(typeof(GameObjectConversionGroup))]
+    public class TMPFontAssetConversionSystem : GameObjectConversionSystem {
+        protected override void OnUpdate() {
+            Entities.ForEach<TMP_FontAsset>((TMP_FontAsset c0) => {
+                var entity = GetPrimaryEntity(c0);
+                SetUpGlyph(entity, c0.glyphLookupTable);
+            });
+        }
+
+        private void SetUpGlyph(Entity entity, in Dictionary<uint, Glyph> map) {
+            var buffer = DstEntityManager.AddBuffer<GlyphElement>(entity);
+            buffer.Capacity = map.Count;
+
+            foreach (var kv in map) {
+                buffer.Add(new GlyphElement {
+                    Char      = (ushort)kv.Key,
+                    Scale     = kv.Value.scale,
+                    GlyphRect = kv.Value.glyphRect,
+                    Metrics   = kv.Value.metrics
+                });
+            }
+        }
+    }
+
+    public class TextMeshProUGUIConversionSystem : GameObjectConversionSystem {
         protected override void OnUpdate() {
             Entities.ForEach((TextMeshProUGUI c0) => {
                 var entity = GetPrimaryEntity(c0);
 
-                DstEntityManager.AddComponentData(entity, new Dimensions           { Value = c0.rectTransform.Int2Size() });
-                DstEntityManager.AddComponentData(entity, new AppliedColor         { Value = c0.color });
+                DstEntityManager.AddComponentData(entity, new Dimensions { Value = c0.rectTransform.Int2Size() });
+                DstEntityManager.AddComponentData(entity, new AppliedColor { Value = c0.color });
                 DstEntityManager.AddSharedComponentData(entity, new RenderMaterial { Value = c0.fontSharedMaterial });
                 AddTextComponent(entity, c0.text);
             });
@@ -19,18 +53,11 @@ namespace UGUIDots.Conversions.Systems {
         private void AddTextComponent(Entity e, string text) {
             var length = text.Length;
 
-            if (text.Length <= 30) {
-                DstEntityManager.AddComponentData(e, new Text32 { Value = text });
-            } else if (text.Length <= 62) {
-                DstEntityManager.AddComponentData(e, new Text64 { Value = text });
-            } else if (text.Length <= 126) {
-                DstEntityManager.AddComponentData(e, new Text128 { Value = text });
-            } else if (text.Length <= 510) {
-                DstEntityManager.AddComponentData(e, new Text512 { Value = text });
-            } else if (text.Length <= 4096) {
-                DstEntityManager.AddComponentData(e, new Text4096 { Value = text });
-            } else {
-                throw new System.ArgumentOutOfRangeException("Text too long.");
+            var buffer = DstEntityManager.AddBuffer<TextElement>(e);
+            buffer.ResizeUninitialized(length);
+
+            for (int i = 0; i < length; i++) {
+                buffer[i] = text[i];
             }
         }
     }
