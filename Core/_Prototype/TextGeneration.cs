99a229b44db911c4a44e1877b36fb9728619279b
Porrith Suong <psuong95@gmail.com>
2020-01-13 17:40:37 -0500

    Adds experimental mesh creation.

--- Core/_Prototype/TextGeneration.cs
+++ Core/_Prototype/TextGeneration.cs
@@ -28,16 +28,25 @@ public class TextGeneration : MonoBehaviour {
         glyphLookUp = FontAsset.glyphLookupTable;
         block.SetColor(ShaderIDConstants.Color, Color.green);
 
-        mesh = new Mesh();
+        Material = Canvas.GetDefaultCanvasMaterial();
+
+        mesh = MeshUtils.Experimental.CreateQuad(300, 300);
+        mesh.RecalculateNormals();
+        mesh.RecalculateBounds();
+
+        mesh.UploadMeshData(false);
     }
 
     void OnDrawGizmos() {
         if (Text.Length == 0) {
             return;
         }
-
         RenderTextQuads(Screen.width / 4, Screen.height / 2, 1);
-        RenderText(Text, 0, 2, 1);
+    }
+
+    void Update() {
+        var m = Matrix4x4.TRS(new Vector3(Screen.width / 2, Screen.height / 2, 0), Quaternion.identity, Vector3.one);
+        Feature.Pass.InstructionQueue.Enqueue((mesh, Material, m, block));
     }
 
     void RenderTextQuads(float x, float y, float scale)
@@ -58,7 +67,7 @@ public class TextGeneration : MonoBehaviour {
             var TR = new Vector3(xPos + width, yPos + height);
             var BR = new Vector3(xPos + width, yPos);
 
-            Debug.Log($"RenderTextQuads: {c}, Bearing Y: {glyph.BearingY(0)}, Bearing X: {glyph.BearingX()}, w: {glyph.Width()}, h: {glyph.Height()}");
+            // Debug.Log($"RenderTextQuads: {c}, Bearing Y: {glyph.BearingY(0)}, Bearing X: {glyph.BearingX()}, w: {glyph.Width()}, h: {glyph.Height()}");
 
             /*
             var mesh = new Mesh();
@@ -102,47 +111,6 @@ public class TextGeneration : MonoBehaviour {
             Gizmos.DrawLine(TL, TR);
             Gizmos.DrawLine(TR, BR);
             Gizmos.DrawLine(BL, BR);
-        }
-    }
-
-    void RenderText(in string txt, float x, float y, float scale) {
-        if (glyphLookUp == null) {
-            Start();
-        }
-
-        for (int i = 0; i < txt.Length; i++) {
-            var c = txt[i];
-
-            var glyph   = glyphLookUp[c];
-            var metrics = glyph.metrics;
-
-            var bearingX = metrics.horizontalBearingX;
-            var bearingY = metrics.horizontalBearingY;
-
-            var advance = metrics.horizontalAdvance;
-
-            var gWidth = metrics.width;
-            var gHeight = metrics.height;
-
-            var xPos = x + bearingX * scale;
-            var yPos = y - (gHeight - bearingY) * scale;
-
-            Debug.Log($"RenderText: {c} Bearing X: {bearingX} Bearing Y: {bearingY}, Width: {gWidth}, Height: {gHeight}");
-
-            var w = gWidth * scale;
-            var h = gHeight * scale;
-
-            var bl = new Vector2(xPos, yPos);
-            var br = new Vector2(xPos + w, yPos);
-            var tl = new Vector2(xPos, yPos + h);
-            var tr = new Vector2(xPos + w, yPos + h);
-
-            Gizmos.DrawLine(bl, br);
-            Gizmos.DrawLine(bl, tl);
-            Gizmos.DrawLine(tl, tr);
-            Gizmos.DrawLine(tr, br);
-
-            x += (advance * Spacing) * (scale);
         }
     }
 }
