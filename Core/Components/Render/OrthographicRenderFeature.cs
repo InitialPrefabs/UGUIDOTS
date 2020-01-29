350425a02b748795e21dc1ab16b8b6d7ec7352aa
Porrith Suong <psuong95@gmail.com>
2020-01-29 17:45:49 -0500

    Tested on Android devices, I think I need to delay the conversion...

--- Core/Components/Render/OrthographicRenderFeature.cs
+++ Core/Components/Render/OrthographicRenderFeature.cs
@@ -10,10 +10,13 @@ namespace UGUIDots.Render {
         public Queue<(Mesh, Material, Matrix4x4, MaterialPropertyBlock)> InstructionQueue { get; private set; }
         private string profilerTag;
 
+        // private CommandBuffer cmd;
+
         public OrthographicRenderPass(OrthographicRenderSettings settings) {
             profilerTag          = settings.ProfilerTag;
             base.renderPassEvent = settings.RenderPassEvt;
             InstructionQueue     = new Queue<(Mesh, Material, Matrix4x4, MaterialPropertyBlock)>();
+            // cmd                  = new CommandBuffer() { name = profilerTag };
         }
 
         public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData) {
@@ -22,29 +25,27 @@ namespace UGUIDots.Render {
             }
 
             // TODO: Need a better way to handle finding the camera
-            if (renderingData.cameraData.camera != null) {
-                var cmd = CommandBufferPool.Get(profilerTag);
-                using (new ProfilingSample(cmd, profilerTag)) {
-                    context.ExecuteCommandBuffer(cmd);
-                    cmd.Clear();
+            var cmd = CommandBufferPool.Get(profilerTag);
+            using (new ProfilingSample(cmd, profilerTag)) {
+                context.ExecuteCommandBuffer(cmd);
+                cmd.Clear();
 
-                    // Set the projection view matrix
-                    var proj = Matrix4x4.Ortho(0, Screen.width, 0, Screen.height, -100f, 100f);
-                    var view = Matrix4x4.TRS(Vector3.zero, Quaternion.identity, Vector3.one);
-                    cmd.SetViewProjectionMatrices(view, proj);
+                // Set the projection view matrix
+                var proj = Matrix4x4.Ortho(0, Screen.width, 0, Screen.height, -100f, 100f);
+                var view = Matrix4x4.TRS(Vector3.zero, Quaternion.identity, Vector3.one);
+                cmd.SetViewProjectionMatrices(view, proj);
 
-                    while (InstructionQueue.Count > 0) {
-                        var tuple = InstructionQueue.Dequeue();
-                        var mesh = tuple.Item1;
+                while (InstructionQueue.Count > 0) {
+                    var tuple = InstructionQueue.Dequeue();
+                    var mesh = tuple.Item1;
 
-                        for (int i = 0; i < mesh.subMeshCount; i++) {
-                            cmd.DrawMesh(mesh, tuple.Item3, tuple.Item2, i, 0, tuple.Item4);
-                        }
+                    for (int i = 0; i < mesh.subMeshCount; i++) {
+                        cmd.DrawMesh(mesh, tuple.Item3, tuple.Item2, i, 0, tuple.Item4);
                     }
                 }
-                context.ExecuteCommandBuffer(cmd);
-                CommandBufferPool.Release(cmd);
             }
+            context.ExecuteCommandBuffer(cmd);
+            CommandBufferPool.Release(cmd);
         }
     }
 
