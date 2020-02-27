2899b1387825a6c17d97ed87d03ed74d041876f0
Porrith Suong <porrith.suong@gmail.com>
2020-02-26 21:57:22 -0500

    Adds a click type component to help determine the kind of click.

---
+++ Controls/Components/ButtonComponents.cs
@@ -0,0 +1,13 @@
+using Unity.Entities;
+
+namespace UGUIDots.Controls {
+
+    public enum ClickType : byte {
+        PressDown = 1 << 0,
+        ReleaseUp = 1 << 1
+    }
+
+    public struct ButtonClickType : IComponentData {
+        public ClickType Value;
+    }
+}
