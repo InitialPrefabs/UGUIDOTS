22891c8b375ed376f3b2e23a93c6f986fbfbda3f
Porrith Suong <psuong95@gmail.com>
2020-03-14 21:22:15 -0400

    Adds preprocessors for InputDataAuthoring.

--- Controls/Authoring/InputDataAuthoring.cs
+++ Controls/Authoring/InputDataAuthoring.cs
@@ -1,4 +1,5 @@
 ﻿using Unity.Entities;
+using Unity.Transforms;
 using UnityEngine;
 
 namespace UGUIDots.Controls.Authoring {
@@ -8,7 +9,15 @@ namespace UGUIDots.Controls.Authoring {
         public KeyCode PrimaryMouseKey = KeyCode.Mouse0;
 
         public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem) {
+#if UNITY_EDITOR || UNITY_STANDALONE
             dstManager.AddComponentData(entity, new PrimaryMouseKeyCode { Value = PrimaryMouseKey });
+#endif
+
+            dstManager.RemoveComponent<LocalToWorld>(entity);
+            dstManager.RemoveComponent<Translation>(entity);
+            dstManager.RemoveComponent<Rotation>(entity);
+            dstManager.RemoveComponent<Scale>(entity);
+            dstManager.RemoveComponent<NonUniformScale>(entity);
         }
     }
 }
