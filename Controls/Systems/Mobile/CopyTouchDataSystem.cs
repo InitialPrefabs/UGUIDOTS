784c15eaf88401d668ddfda5c3440e6b2d9e2c06
Porrith Suong <psuong95@gmail.com>
2020-03-16 21:53:08 -0400

    Adds a touch struct but the logic for checking collisions doesn't work yet.

---
+++ Controls/Systems/Mobile/CopyTouchDataSystem.cs
@@ -0,0 +1,26 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace UGUIDots.Controls.Systems {
+
+    /// <summary>
+    /// Copies data from the Input engine into a TouchElement buffer.
+    /// </summary>
+    [UpdateInGroup(typeof(InputGroup))]
+    public class CopyTouchDataSystem : SystemBase {
+
+        protected override void OnUpdate() {
+            Entities.ForEach((DynamicBuffer<TouchElement> b0) => {
+                var touchBuffer = b0.AsNativeArray();
+                for (int i = 0; i < Input.touchCount; i++) {
+                    touchBuffer[i] = Input.GetTouch(i);
+                    // Debug.Log($"At {i}: {touchBuffer[i].Phase}");
+                }
+
+                for (int i = Input.touchCount; i < touchBuffer.Length; i++) {
+                    touchBuffer[i] = TouchElement.Default();     
+                }
+            }).WithoutBurst().Run();
+        }
+    }
+}
