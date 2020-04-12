49bcb84f25ca943e50caa81da222439bc62074ab
Porrith Suong <psuong95@gmail.com>
2020-04-12 13:49:28 -0400

    Adds a basic instanced unlit shader.

---
+++ Shaders/UnlitTranslation.hlsl
@@ -0,0 +1,62 @@
+#ifndef UGUIDOTS_UNLIT_TRANSLATION
+#define UGUIDOTS_UNLIT_TRANSLATION
+
+#include "Common.hlsl"
+
+TEXTURE2D(_MainTex);
+SAMPLER(sampler_MainTex);
+
+UNITY_INSTANCING_BUFFER_START(UnityPerMaterial)
+    UNITY_DEFINE_INSTANCED_PROP(float4, _MainTex_ST);
+    UNITY_DEFINE_INSTANCED_PROP(float4, _BaseColor);
+    UNITY_DEFINE_INSTANCED_PROP(float, _Cutoff);
+UNITY_INSTANCING_BUFFER_END(UnityPerMaterial);
+
+struct Attributes 
+{
+    float3 positionOS: POSITION;
+    float4 color:      COLOR;
+    float2 baseUV:     TEXCOORD0;
+};
+
+struct Varyings 
+{
+    float4 positionCS: SV_POSITION;
+    float4 color:      COLOR;
+    float2 baseUV:     VAR_BASE_UV;
+
+    UNITY_VERTEX_INPUT_INSTANCE_ID
+};
+
+Varyings UnlitPassVertex(Attributes input)
+{
+    Varyings output;
+    UNITY_SETUP_INSTANCE_ID(input);
+    UNITY_TRANSFER_INSTANCE_ID(input, ouput);
+    float3 positionWS = TransformObjectToWorld(input.positionOS);
+    output.positionCS = TransformWorldToHClip(positionWS);
+
+    // TODO: Sample the texture color
+    float4 baseST = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _MainTex_ST);
+    output.baseUV = input.baseUV * baseST.xy + baseST.zw;
+
+    output.color = input.color;
+
+    return output;
+}
+
+float4 UnlitPassFragment(Varyings input) : SV_TARGET
+{
+    UNITY_SETUP_INSTANCE_ID(input);
+    float4 baseMap = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, input.baseUV);
+    float4 baseColor = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _BaseColor);
+    float4 base = baseMap * baseColor * input.color;
+
+#if defined (_CLIPPING)
+    clip(base.a - UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _Cutoff));
+#endif
+
+    return base;
+}
+
+#endif
---
+++ Shaders/UnlitTranslation.shader
@@ -0,0 +1,30 @@
+﻿Shader "UGUIDOTS/Unlit/UnlitTranslationShader"
+{
+    Properties
+    {
+        _MainTex("Texture", 2D) = "white" {}
+        _BaseColor("Color", color) = (1.0, 1.0, 1.0, 1.0)
+        _Cutoff ("Alpha Cutoff", Range(0.0, 1.0)) = 0.5
+        [Toggle(_CLIPPING)] _Clipping ("Alpha Clipping", Float) = 1
+        [Enum(UnityEngine.Rendering.BlendMode)] _SrcBlend ("Src Blend", Float) = 1
+        [Enum(UnityEngine.Rendering.BlendMode)] _DstBlend ("Dst Blend", Float) = 0
+        [Enum(Off, 0, On, 1)] _ZWrite ("Z Write", Float) = 1
+    }
+
+    SubShader
+    {
+        Pass 
+        {
+            Blend [_SrcBlend] [_DstBlend]
+            ZWrite [_ZWrite]
+
+            HLSLPROGRAM
+            #pragma shader_feature _CLIPPING
+            #pragma multi_compile_instancing
+            #pragma vertex UnlitPassVertex
+            #pragma fragment UnlitPassFragment
+            #include "UnlitTranslation.hlsl"
+            ENDHLSL
+        }
+    }
+}
