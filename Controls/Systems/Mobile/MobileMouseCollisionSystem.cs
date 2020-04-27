0a19fcffd5b044a09654123c524fc4b4d73d21cd
Porrith Suong <psuong95@gmail.com>
2020-04-27 19:21:20 -0400

    Uses stackalloc instead of Temp allocs for input data.

--- Controls/Systems/Mobile/MobileMouseCollisionSystem.cs
+++ Controls/Systems/Mobile/MobileMouseCollisionSystem.cs
@@ -1,5 +1,4 @@
-﻿using Unity.Collections;
-using Unity.Collections.LowLevel.Unsafe;
+﻿using Unity.Collections.LowLevel.Unsafe;
 using Unity.Mathematics;
 using Unity.Entities;
 using Unity.Transforms;
@@ -12,21 +11,24 @@ namespace UGUIDots.Controls.Systems {
 
         protected unsafe override void OnUpdate() {
 
-            var touches = new NativeArray<TouchElement>(10, Allocator.Temp);
+            // TODO: This constantly allocates 10 elements every update, check if there is a better way
+            var touches = stackalloc TouchElement[10];
+            var size    = stackalloc int[1];
 
             Entities.ForEach((DynamicBuffer<TouchElement> b0) => {
-                UnsafeUtility.MemCpy(touches.GetUnsafePtr(), b0.GetUnsafePtr(), 
-                    UnsafeUtility.SizeOf<TouchElement>() * b0.Length);
-            }).Run();
+                UnsafeUtility.MemCpy(touches, b0.GetUnsafePtr(), UnsafeUtility.SizeOf<TouchElement>() * b0.Length);
+                size[0] = b0.Length;
+            }).WithNativeDisableUnsafePtrRestriction(touches).WithNativeDisableUnsafePtrRestriction(size).Run();
 
-            Entities.WithDeallocateOnJobCompletion(touches).WithNone<ButtonDisabledTag>().
+            Entities.WithNativeDisableUnsafePtrRestriction(touches).WithNativeDisableUnsafePtrRestriction(size).
+                WithNone<ButtonDisabledTag>().
                 ForEach((ref ClickState c0, ref ButtonVisual c1, in Dimensions c2, in LocalToWorld c3, in ButtonClickType c4) => {
                 var aabb = new AABB {
                     Center  = c3.Position,
                     Extents = new float3(c2.Extents(), c3.Position.z)
                 };
 
-                for (int i = 0; i < touches.Length; i++) {
+                for (int i = 0; i < size[0]; i++) {
                     var touch = touches[i];
                     if (aabb.Contains(new float3(touches[i].Position, c3.Position.z))) {
                         switch (c4.Value) {
