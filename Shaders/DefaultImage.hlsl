15bab3d428dabc090f2bdea5593122dff81251b4
Porrith Suong <psuong95@gmail.com>
2020-05-08 10:25:13 -0400

    Fix instancing syntax error.

--- Shaders/DefaultImage.hlsl
+++ Shaders/DefaultImage.hlsl
@@ -7,18 +7,20 @@ TEXTURE2D(_MainTex);
 SAMPLER(sampler_MainTex);
 
 UNITY_INSTANCING_BUFFER_START(UnityPerMaterial)
-    UNITY_DEFINE_INSTANCED_PROP(float, _Fill);
-    UNITY_DEFINE_INSTANCED_PROP(float, _FillType);
-    UNITY_DEFINE_INSTANCED_PROP(float4, _Translation);
-    UNITY_DEFINE_INSTANCED_PROP(float4, _MainTex_ST);
-    UNITY_DEFINE_INSTANCED_PROP(float4, _BaseColor);
-UNITY_INSTANCING_BUFFER_END(UnityPerMaterial);
+    UNITY_DEFINE_INSTANCED_PROP(float, _Fill)
+    UNITY_DEFINE_INSTANCED_PROP(float, _FillType)
+    UNITY_DEFINE_INSTANCED_PROP(float4, _Translation)
+    UNITY_DEFINE_INSTANCED_PROP(float4, _MainTex_ST)
+    UNITY_DEFINE_INSTANCED_PROP(float4, _BaseColor)
+UNITY_INSTANCING_BUFFER_END(UnityPerMaterial)
 
 struct Attributes 
 {
     float3 positionOS: POSITION;
     float4 color:      COLOR;
     float2 baseUV:     TEXCOORD0;
+
+    UNITY_VERTEX_INPUT_INSTANCE_ID
 };
 
 struct Varyings 
@@ -34,8 +36,8 @@ Varyings UnlitPassVertex(Attributes input)
 {
     Varyings output;
     UNITY_SETUP_INSTANCE_ID(input);
-    UNITY_TRANSFER_INSTANCE_ID(input, ouput);
-    float3 positionWS = TransformObjectToWorld(input.positionOS + _Translation.xyz);
+    UNITY_TRANSFER_INSTANCE_ID(input, output);
+    float3 positionWS = TransformObjectToWorld(input.positionOS + UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _Translation).xyz);
     output.positionCS = TransformWorldToHClip(positionWS);
 
     float4 baseST = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _MainTex_ST);
