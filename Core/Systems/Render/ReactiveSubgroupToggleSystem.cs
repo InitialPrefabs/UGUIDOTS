5bbd71fda74ae70ff116159497a0d385ed8e008e
Porrith Suong <psuong95@gmail.com>
2020-05-23 11:39:06 -0400

    Removes unnecessary UpdateVertexColorTags.

--- Core/Systems/Render/ReactiveSubgroupToggleSystem.cs
+++ Core/Systems/Render/ReactiveSubgroupToggleSystem.cs
@@ -7,14 +7,33 @@ namespace UGUIDots.Render.Systems {
     /// </summary>
     [UpdateInGroup(typeof(PresentationSystemGroup))]
     public class ReactiveSubgroupToggleSystem : SystemBase {
+
+        private EntityQuery disabledUpdatesQuery;
         private EntityCommandBufferSystem cmdBufferSystem;
 
         protected override void OnCreate() {
+            disabledUpdatesQuery = GetEntityQuery(new EntityQueryDesc {
+                None = new [] {
+                    ComponentType.ReadOnly<LocalTriangleIndexElement>(), ComponentType.ReadOnly<LocalVertexData>()
+                },
+                Any = new [] { 
+                    ComponentType.ReadOnly<UpdateVertexColorTag>(), ComponentType.ReadOnly<Disabled>()
+                },
+                Options = EntityQueryOptions.IncludeDisabled
+            });
             cmdBufferSystem = World.GetOrCreateSystem<BeginPresentationEntityCommandBufferSystem>();
         }
 
         protected override void OnUpdate() {
             var cmdBuffer = cmdBufferSystem.CreateCommandBuffer().ToConcurrent();
+
+            var disabled = GetComponentDataFromEntity<Disabled>(true);
+            var update   = GetComponentDataFromEntity<UpdateVertexColorTag>(true);
+
+            Dependency = Entities.WithStoreEntityQueryInField(ref disabledUpdatesQuery).
+                ForEach((Entity entity, int entityInQueryIndex) => {
+                cmdBuffer.RemoveComponent<UpdateVertexColorTag>(entityInQueryIndex, entity);
+            }).ScheduleParallel(Dependency);
 
             Dependency = Entities.WithAll<EnableRenderingTag>().ForEach((Entity entity, int entityInQueryIndex) => {
                 cmdBuffer.RemoveComponent<EnableRenderingTag>(entityInQueryIndex, entity);
