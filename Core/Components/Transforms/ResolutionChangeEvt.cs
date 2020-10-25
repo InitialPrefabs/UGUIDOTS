3f3d9ef257e3be838f0c230d16188da6f5fc8fe3
Porrith <porrith.suong@gmail.com>
2020-10-25 18:16:51 -0400

    Removes the old change event systems and dependencies.

--- Core/Components/Transforms/ResolutionChangeEvt.cs
+++ Core/Components/Transforms/ResolutionChangeEvt.cs
@@ -1,25 +1,6 @@
-using System;
 using Unity.Entities;
-using Unity.Mathematics;
 
 namespace UGUIDOTS.Transforms {
 
     public struct OnResolutionChangeTag : IComponentData { }
-
-    /// <summary>
-    /// Marks a messaging entity that the resolution has changed.
-    /// </summary>
-    public struct ResolutionEvent : IComponentData, IEquatable<ResolutionEvent> {
-        public int2 Value;
-
-        public override int GetHashCode() {
-            return Value.GetHashCode();
-        }
-
-        public bool Equals(ResolutionEvent other) {
-            return other.Value.Equals(Value);
-        }
-    }
-
-    public struct RescaleDimension : IComponentData { }
 }
