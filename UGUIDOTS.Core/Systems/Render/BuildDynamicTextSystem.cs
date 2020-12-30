4048ee1a736c9d3da48c9373f34480d020443646
Porrith <porrith.suong@gmail.com>
2020-12-29 19:02:40 -0500

    Separates the BuildDynamicTextJob into its own file.

---
+++ UGUIDOTS.Core/Systems/Render/BuildDynamicTextSystem.cs
@@ -0,0 +1,15 @@
+using Unity.Entities;
+
+namespace UGUIDOTS.Render.Systems {
+
+    [UpdateInGroup(typeof(SimulationSystemGroup), OrderLast = true)]
+    [UpdateBefore(typeof(BuildRenderHierarchySystem))]
+    [DisableAutoCreation]
+    public unsafe class BuildDynamicTextSystem : SystemBase {
+        protected override void OnUpdate() {
+            // TODO: Collect the entities
+            // TODO: Figure out the static spans
+            // TODO: Apply the mesh slice update to dynamic text.
+        }
+    }
+}
